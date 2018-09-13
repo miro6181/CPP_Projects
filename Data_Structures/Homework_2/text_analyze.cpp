1485e06182bb64ce54b22a69c15b3363bedccf9b
Michael <mike.rogers1272@gmail.com>
2018-09-12 21:55:23 -0600

    Homework 2 changes

--- Data_Structures/Homework_2/text_analyze.cpp
+++ Data_Structures/Homework_2/text_analyze.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+// void​ ​getStopWords​(​char​ *ignoreWordFileName, ​string​ ignoreWords[]);
+//
+// bool​ ​isStopWord​(​string​ word, ​string​ ignoreWords[]);
+// bool​ ​isStopWord​(​string​ word, ​string​ ignoreWords[]) {
+//   for(i = 0; i < )
+// }
+//
+//
+// int​ ​getTotalNumberNonStopWords​(wordItem ​list​[], ​int​ length);
+//
+// void​ ​arraySort​(wordItem ​list​[], ​int​ length);
+//
+// void​ ​printTopN​(wordItem wordItemList[], ​int​ topN);
+
 int main(int argc, char *argv[]) {
-  
+  if(argc != 3) {
+    cout << "Invalid number of arguments." << endl;
+  }
+  else {
+    //Execute program
+  }
 }
