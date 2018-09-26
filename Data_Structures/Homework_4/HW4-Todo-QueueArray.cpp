07e9c39cb08a40e1077561d0ed59fbdd57d62218
Michael <mike.rogers1272@gmail.com>
2018-09-25 23:29:20 -0600

    Changes to Homework 4 Queue Array and main.cpp. Neither work at the time of this commit

--- Data_Structures/Homework_4/HW4-Todo-QueueArray.cpp
+++ Data_Structures/Homework_4/HW4-Todo-QueueArray.cpp
@@ -2,3 +2,65 @@
 #include "HW4-Todo-QueueArray.hpp"
 
 using namespace std;
+
+TodoQueueArray::TodoQueueArray() {
+  queueEnd = -1;
+  queueFront = -1;
+  //Constructor
+}
+
+bool TodoQueueArray::isEmpty() {
+  return (queueEnd == -1 && queueFront == -1);
+}
+
+bool TodoQueueArray::isFull() {
+  return (queueEnd + 1)%MAX_QUEUE_SIZE == queueFront ? true : false;
+}
+
+void TodoQueueArray::enqueue(string todoItem) {
+  cout << "Enqueueing: " << todoItem << endl;
+  if(isFull()) {
+    cout << "Error: Queue is Full";
+    return;
+  }
+  if(isEmpty()) {
+    queueFront = queueEnd = 0;
+  }
+  else {
+    queueEnd = (queueEnd + 1)%MAX_QUEUE_SIZE;
+  }
+  queue[queueEnd] = todoItem;
+}
+
+void TodoQueueArray::dequeue() {
+  cout << "Dequeuing \n";
+  if(isEmpty()) {
+    cout << "Error: Queue is Empty\n";
+    return;
+  }
+  else if(queueFront == queueEnd) {
+    queueEnd = queueFront = -1;
+  }
+  else {
+    queueFront = (queueFront + 1)%MAX_QUEUE_SIZE;
+  }
+}
+
+TodoItem TodoQueueArray::peek() {
+  if(queueFront == -1) {
+    cout << "Error: Cannot return front of empty queue.\n";
+    return -1;
+  }
+  return queue[queueFront];
+}
+
+int getQueueFront() {
+  return queue[queueFront];
+}
+int getQueueEnd() {
+  return queue[queueEnd];
+}
+
+TodoItem* getQueue() {
+  return queue;
+}
--- Data_Structures/Homework_4/main.cpp
+++ Data_Structures/Homework_4/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-// #include "HW4-Todo-QueueArray.cpp"
-// #include "HW4-Todo-QueueLinkedList.cpp"
-// #include "HW4-Todo-StackArray.cpp"
-// #include "HW4-Todo-StackLinkedList.cpp"
+// #include "HW4-Todo-QueueArray.hpp"
+// #include "HW4-Todo-QueueLinkedList.hpp"
+// #include "HW4-Todo-StackArray.hpp"
+// #include "HW4-Todo-StackLinkedList.hpp"
 
 using namespace std;
 
 int main() {
-  
+  // Queue Array Test
+  // TodoQueueArray Q;
+  //
+  // Q.enqueue("wash dog"); Q.getQueueFront(); Q.getQueueEnd();
+  // Q.enqueue("trash"); Q.getQueueFront(); Q.getQueueEnd();
+  // Q.enqueue("homework"); Q.getQueueFront(); Q.getQueueEnd();
+  // Q.dequeue(); Q.getQueueFront(); Q.getQueueEnd();
+  // Q.enqueue("school"); Q.getQueueFront(); Q.getQueueEnd();
 }
