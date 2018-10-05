c96b0ef0a41499fae8281f55ea2550f42ded7a1f
Michael <mike.rogers@gmail.com>
2018-10-05 16:09:05 -0600

    Class notes and Exam 1

---
+++ Data_Structures/Exam_1/exam_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namspace std;
+
+//Writing to a text file
+// int main () {
+//   ofstream myfile; //instead of ifstream(used for reading)
+//   myfile.open ("example.txt"); //Open txt file
+//   myfile << "Writing this to a file.\n"; //similiar to cout <<
+//   myfile.close(); //Close File
+//   return 0;
+// }
+
+int main() {
+  
+}
