5f6ff8e009ef7ef3d7044b060803457dc47ca0a8
Michael <mike.rogers@gmail.com>
2018-10-28 20:48:57 -0600

    Homework 7

--- Data_Structures/Homework_7/HungerGamesHash.cpp
+++ Data_Structures/Homework_7/HungerGamesHash.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <ssteam>
 #include "HashTable.hpp"
 
 using namespace std;
@@ -12,12 +14,30 @@ HashTable::~HashTable() {
 }
 
 void HashTable::getStopWords(char *ignoreWordFileName) {
-
-}
+  ifstream inFile(ignoreWordFileName);
+  if(inFile.is_open()) {
+    string line;
+    while(getline(inFile, line)) {
+      stringstream ss;
+      ss << line;
+      string item;
+      getline(ss,item);
+      vecIgnoreWords.push_back(line);
+    }
+  }
+} //Done
 
 bool HashTable::isStopWord(string word) {
-
-}
+  for(int i = 0; i < STOPWORD_LIST_SIZE; i++) {
+    if(word == vecIgnoreWords[i]){
+      return true;
+    }
+    else {
+      continue;
+    }
+  }
+  return false;
+} //Done
 
 bool HashTable::isInTable(string word) {
 
@@ -28,15 +48,54 @@ void HashTable::incrementCount(string word) {
 }
 
 void HashTable::addWord(string word) {
-
-}
+  int hash_val = getHash(word);
+  wordItem* prev = NULL;
+  wordItem* entry = hashTable[hash_val];
+  while (entry != NULL) {
+    prev = entry;
+    entry = entry->next;
+  }
+  if (entry == NULL) {
+    entry = new wordItem;
+    entry->count = 1;
+    entry->word = word;
+    entry ->next = NULL;
+    if (prev == NULL) {
+      hashTable[hash_val]= entry;
+    }
+    else {
+      prev->next = entry;
+    }
+  }
+  incrementCount(word);
+  entry->word = word;
+} //Done
 
 int HashTable::getTotalNumberNonStopWords() {
 
 }
 
 void HashTable::printTopN(int n) {
-
+  vector<wordItem*> TopN;
+  wordItem* temp;
+  for (int i = 0; i < hashTableSize; i++) {
+    temp = hashTable[i];
+    while (temp != NULL) {
+      for (int j = 0; j < n; j++) {
+        if(arr[j]->count <= temp->count && arr[j+1]->count <= temp->count) {
+          for (int k = 0; k < j; k++) {
+            arr[k] = arr[k+1]
+          }
+          arr[j]=arr[j+1];
+          arr[j] = temp;
+        }
+      }
+      temp = temp->next;
+    }
+  }
+  for (int k = 0; k < n; k++) {
+    std::cout << arr[k]->word << "--" << arr[k]->count;
+  }
 }
 
 int HashTable::getNumUniqueWords() {
@@ -58,7 +117,7 @@ int HashTable::getHash(string word) { //Private Member function
     hash += hashTableSize;
   }
   return hash;
-}
+} //Done
 
 wordItem* HashTable::searchTable(string word){ //Private Member function
 
--- Data_Structures/Homework_7/main.cpp
+++ Data_Structures/Homework_7/main.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "HashTable.hpp"
 
 using namespace std;
 
-int main(int argc, char argv[]) {
-  if(argc != 4) {
-    cout << "Invalid number of arguments." << endl;
-  }
-  else {
-    
-  }
+int main(int argc, char* argv[]) {
+
+    HashTable HungerGamesTable = HashTable(stoi(argv[4]));
+    HungerGamesTable.getStopWords(argv[3]);
+
+    ifstream wordStream;
+    wordStream.open(argv[2]);
+    //cout << argv[2] << endl;
+    string line;
+    //cout << "chk1" << endl;
+    while(wordStream >> line) {
+        if(HungerGamesTable.isInTable(line) && HungerGamesTable.isStopWord(line)) {
+            HungerGamesTable.incrementCount(line);
+        }
+        else if(!HungerGamesTable.isStopWord(line)) {
+            HungerGamesTable.addWord(line);
+        }
+    }
+
+    HungerGamesTable.printTopN(stoi(argv[1]));
+
+    cout << "#" << endl;
+    cout << "Number of collisions: " << HungerGamesTable.getNumCollisions() << endl;
+    cout << "#" << endl;
+    cout << "Unique non-stop words: " << HungerGamesTable.getNumUniqueWords() << endl;
+    cout << "#" << endl;
+    cout << "Total non-stop words: " << HungerGamesTable.getTotalNumberNonStopWords() << endl;
 }
