25ad4543372d8d3890e4bbd0e2883fb080cddfd0
Michael <mike.rogers@gmail.com>
2018-11-23 16:11:48 -0700

    Final project changes

--- Data_Structures/Final_Project/CRM.cpp
+++ Data_Structures/Final_Project/CRM.cpp
@@ -3,40 +3,58 @@
 
 using namespace std;
 
+//Helper Functions
+void deleteTree(CRMTree *root) {
+
+}
 // CustNode Memeber Functions
 CustNode::CustNode() {
+  parent = rightChild = leftChild = nullptr;
+} // Done
 
-}
 CustNode::CustNode(std::string n, std::string e, std::string p, std::vector<transaction> t) {
+  name = n;
+  email = e;
+  phone = p;
+  parent = rightChild = leftChild = nullptr;
+} // Done
 
-}
 CustNode::~CustNode() {
 
 }
+
 void CustNode::addTransaction(std::string description, double amount) {
 
 }
 
 // CRMTree Member Functions
+
+//Constructor
 CRMTree::CRMTree() {
+  root = NULL;
+} // Done
 
-}
+//Destructor
 CRMTree::~CRMTree() {
+  deleteTree(root);
+} // Done
 
-}
 void CRMTree::addCustomer(std::string name,std::string email, std::string phone) {
 
 }
+
 void CRMTree::deleteCustomer(CustNode delCust) {
 
 }
+
 void CRMTree::newTransaction(CustNode newTransNode, std::string description, double amount) {
 
 }
+
 void CRMTree::findCustomer() {
 
 }
+
 CustNode CRMTree::search(CustNode inCust) {
 
 }
-l, 
