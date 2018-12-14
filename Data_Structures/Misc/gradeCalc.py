d0bf2ece2a6377252df64b173178d540b3228360
Michael <mike.rogers@gmail.com>
2018-12-13 19:01:38 -0700

    Grade Calc Changes

--- Data_Structures/Misc/gradeCalc.py
+++ Data_Structures/Misc/gradeCalc.py
@@ -5,7 +5,7 @@ def gradeCalc(hws, recitaion, midterm_1, midterm_2, project):
         hw_sum += hw
     hw_avg = hw_sum/(len(hws) * 100)
 
-    quiz_avg = 0.7505
+    quiz_avg = 0.80
 
     grade = (0.45 * hw_avg) + (0.1 * quiz_avg) + (0.1 * (midterm_1/100)) + (0.1 * (midterm_2/100)) + (0.1 * (project/100) + (0.15 * recitaion/100))
 
@@ -38,4 +38,4 @@ def gradeCalc(hws, recitaion, midterm_1, midterm_2, project):
 
     return "Your Grade is: " + letter + " (" + str((float(grade) * 100)) + " %" + ")"
 
-print(gradeCalc([85.00, 70.00, 100.00, 40.00, 48.00, 49.67, 75.00, 100.00, 45.00], 90, 95, 65, 100))
+print(gradeCalc([85.00, 70.00, 100.00, 40.00, 48.00, 49.67, 75.00, 100.00, 45.00], 91.67, 95, 65, 100))
